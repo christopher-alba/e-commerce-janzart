8fca2a87230b90b42a236ba3d032c98a3fe20b30
albasychris <albasychris@gmail.com>
2020-04-11 11:56:09 +1200

    set up initial routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react"
+import { Route, withRouter } from "react-router-dom"
 
-const App = () => {
+const App = withRouter(({ location }) => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+      {location.pathname !== "/login" && location.pathname !== "/signup" && (
+        <Navbar />
+      )}
+      <Route exact path='/' component={Home} />
+      <Route exact path='/products' component={Products} />
+      <Route exact path='/products/:id' component={Product} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/signup' component={SignUp} />
+      <Route exact path='/cart' component={Cart} />
+      <Route exact path='/profile' component={Profile} />
+      <Route path='/' component={Footer} />
+      {location.pathname !== "/login" && location.pathname !== "/signup" && (
+        <Footer />
+      )}
+    </>
   )
-}
+})
 
 export default App
