53b90337998710d5c81ef8f00482363f2fdeef82
albasychris <albasychris@gmail.com>
2020-05-01 21:59:11 +1200

    update environment variable in db.js

--- client/api/index.js
+++ client/api/index.js
@@ -1,112 +1,95 @@
 import request from 'superagent'
 
-export function getProducts(searchFilter, catFilter) {
+export function getProducts (searchFilter, catFilter) {
+  return request.get('/products')
+    .then(products => {
+      // console.log(products);
 
-    return request.get('/products')
-        .then(products => {
-            // console.log(products);
+      // console.log(searchFilter)
+      let useSearchFilter = true
+      let useCatFilter = true
+      let productsArray = products.body
+      if (searchFilter === undefined || searchFilter[0] === '') {
+        useSearchFilter = false
+      }
+      if (catFilter === undefined || catFilter.length === 0) {
+        useCatFilter = false
+      }
 
+      // console.log(useSearchFilter, useCatFilter);
 
-            // console.log(searchFilter)
-            let useSearchFilter = true
-            let useCatFilter = true
-            let productsArray = products.body
-            if (searchFilter === undefined || searchFilter[0] === "") {
-                useSearchFilter = false
+      let filteredArray = productsArray.filter(product => {
+        let match1 = false
+        let match2 = false
 
+        if (useSearchFilter === true) {
+          searchFilter.forEach(filter => {
+            // console.log(filter);
 
-
+            filter = filter.toLowerCase()
+            let productName = product.itemName.toLowerCase()
+            let productCats = product.categories.map(cat => cat.toLowerCase())
+            if (
+              productName.includes(filter) || productCats.includes(filter)
+            ) {
+              match1 = true
             }
-            if (catFilter === undefined || catFilter.length === 0) {
-                useCatFilter = false
-            }
-
-            // console.log(useSearchFilter, useCatFilter);
-
-
-
-            let filteredArray = productsArray.filter(product => {
-                let match1 = false
-                let match2 = false
-
-                if (useSearchFilter === true) {
-                    searchFilter.forEach(filter => {
-                        // console.log(filter);
-
-                        filter = filter.toLowerCase()
-                        let productName = product.itemName.toLowerCase()
-                        let productCats = product.categories.map(cat => cat.toLowerCase())
-                        if (
-                            productName.includes(filter) || productCats.includes(filter)
-                        ) {
-                            match1 = true
-                        }
-                    })
-                }
-                else {
-                    // console.log("match1 true");
-                    match1 = true
-                }
-                if (useCatFilter === true) {
-
-                    catFilter.forEach(filter => {
-                        filter = filter.toLowerCase()
-                        let productCats = product.categories.map(cat => cat.toLowerCase())
-                        if (
-                            productCats.includes(filter)
-                        ) {
-                            match2 = true
-                        }
-                    })
-                }
-                else {
-                    match2 = true
-                    // console.log("match2 true!");
-
-                }
-
-                return match1 && match2
-            })
-
-
-
-            return filteredArray
-
-
-        })
-        .catch(errorHandler('GET', '/products'))
-}
-
-export function getProduct(id) {
-    return request.get(`/product/${id}`)
-        .then(product => product)
-        .catch(errorHandler('GET', `/product/${id}`))
-}
-export function startCheckout(items) {
-    return request.post('/product/checkout')
-        .send({ items })
-        .then(res => {
-            console.log(res);
-
-            return res.body
-        })
-}
-export function getToken() {
-    return request.get('/api/v1/auth/external')
-        .then(res => {
-            console.log(JSON.parse(res.text));
-
-            return JSON.parse(res.text)
-
-        })
-
-}
-function errorHandler(method, route) {
-    return (err) => {
-        if (err.message === 'Not Found') {
-            throw Error(`Error: You need to implement an API route for ${method} ${route}`)
+          })
         } else {
-            throw Error(`${err.message} on ${method} ${route}`)
+          // console.log("match1 true");
+          match1 = true
         }
+        if (useCatFilter === true) {
+          catFilter.forEach(filter => {
+            filter = filter.toLowerCase()
+            let productCats = product.categories.map(cat => cat.toLowerCase())
+            if (
+              productCats.includes(filter)
+            ) {
+              match2 = true
+            }
+          })
+        } else {
+          match2 = true
+          // console.log("match2 true!");
+        }
+
+        return match1 && match2
+      })
+
+      return filteredArray
+    })
+    .catch(errorHandler('GET', '/products'))
+}
+
+export function getProduct (id) {
+  return request.get(`/product/${id}`)
+    .then(product => product)
+    .catch(errorHandler('GET', `/product/${id}`))
+}
+export function startCheckout (items) {
+  return request.post('/product/checkout')
+    .send({ items })
+    .then(res => {
+      console.log(res)
+
+      return res.body
+    })
+}
+export function getToken () {
+  return request.get('/api/v1/auth/external')
+    .then(res => {
+      console.log(JSON.parse(res.text))
+
+      return JSON.parse(res.text)
+    })
+}
+function errorHandler (method, route) {
+  return (err) => {
+    if (err.message === 'Not Found') {
+      throw Error(`Error: You need to implement an API route for ${method} ${route}`)
+    } else {
+      throw Error(`${err.message} on ${method} ${route}`)
     }
+  }
 }
--- server/db/db.js
+++ server/db/db.js
@@ -1,19 +1,18 @@
-
-const environment = process.env.ENVIRONMENT || 'development'
-console.log(environment);
+const environment = process.env.NODE_ENV || 'development'
+console.log(environment)
 
 const config = require('../../knexfile.js')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-    getAllItems,
-    getItem
+  getAllItems,
+  getItem
 }
 
-function getAllItems(db = connection) {
-    return db('Items').select()
+function getAllItems (db = connection) {
+  return db('Items').select()
 }
 
-function getItem(id, db = connection) {
-    return db('Items').where('id', id).select().first()
+function getItem (id, db = connection) {
+  return db('Items').where('id', id).select().first()
 }
