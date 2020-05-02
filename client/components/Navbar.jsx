6975fb6332f00e71b9034b68ac8ac4186b2854df
albasychris <albasychris@gmail.com>
2020-05-02 21:42:22 +1200

    fix navbar filters

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React, { Component, useState, useEffect } from 'react'
 import { Responsive, Segment, Dropdown } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
@@ -6,8 +7,8 @@ import CatDropdown from './CatDropdown'
 // authentication
 import { useAuth0 } from '../react-auth0-spa'
 
-let searchQuery = ''
-let catFilter = []
+let searchQueryGlobal = ''
+let catFilterGlobal = []
 
 export default function Navbar (props) {
   // states
@@ -36,12 +37,12 @@ export default function Navbar (props) {
     setMenuDisplayed(!menuDisplayed)
   }
   function updateSearch (evt) {
-    searchQuery = evt.target.value // to be exported as a global variable
+    searchQueryGlobal = evt.target.value // to be exported as a global variable
     setSearchQuery(evt.target.value)
   }
 
   function updateFilter (filters) {
-    catFilter = filters // to be exported as a global variable
+    catFilterGlobal = filters // to be exported as a global variable
     setCatFilter(filters)
   }
   function handleCategories () {
@@ -248,262 +249,6 @@ export default function Navbar (props) {
   )
 }
 
-// class Navbar extends Component {
-// 	constructor(props) {
-// 		super(props)
-// 		state = {
-// 			menuDisplayed: false,
-// 			searchQuery: '',
-// 			currentPage: '',
-// 			catFilter: [],
-// 			catDisplayed: false,
-// 		}
-// 		const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
-// 		this.isAuthenticated = isAuthenticated
-// 		this.loginWithRedirect = loginWithRedirect
-// 		this.logout = logout
-// 	}
-
-// 	handleClick = () => {
-// 		this.setState({ menuDisplayed: !this.menuDisplayed })
-// 	}
-// 	handleCategories = () => {
-// 		this.setState({ catDisplayed: !this.state.catDisplayed })
-// 	}
-// handleLinkClick = evt => {
-// 	this.setState({
-// 		currentPage: evt.target.name,
-// 	})
-// }
-// 	componentDidMount() {
-// 		this.setState({
-// 			currentPage: this.props.pathName.split('/')[1],
-// 		})
-// 	}
-// 	componentDidUpdate(prevProps, prevState, snapshot) {
-// 		if (this.props.pathName !== prevProps.pathName) {
-// 			this.setState({
-// 				currentPage: this.props.pathName.split('/')[1],
-// 			})
-// 		}
-// 	}
-// 	updateSearch = evt => {
-// 		console.log(this.state.searchQuery)
-// 		searchQuery = evt.target.value
-// 		this.setState({ searchQuery: evt.target.value })
-// 	}
-// 	updateFilter = filters => {
-// 		catFilter = filters
-// 		console.log(catFilter)
-// 		this.setState({ catFilter: filters })
-// 	}
-// 	render() {
-// 		return (
-// 			<div className='responsiveNav' style={{ opacity: this.props.animation }}>
-// 				<Responsive as={Segment} minWidth={1000}>
-// 					<div className='navbar'>
-// 						<div className='navbarImg'></div>
-// 						<div className='container'>
-// 							<div className='nav-brand'>JANZART</div>
-
-// 							<div className='nav-end'>
-// 								<Link className='login' to='/cart'>
-// 									<i className='fas fa-shopping-cart nav-cart'></i>
-// 								</Link>
-// 								{!isAuthenticated && (
-// 									<button
-// 										className='login'
-// 										onClick={() => this.loginWithRedirect({})}>
-// 										Log in
-// 									</button>
-// 								)}
-// 								{isAuthenticated && (
-// 									<button className='login' onClick={() => this.logout()}>
-// 										Log out
-// 									</button>
-// 								)}
-// 								<Link className='login' to='/signup'>
-// 									<div className='signup'>SIGN UP</div>
-// 								</Link>
-// 							</div>
-// 						</div>
-// 					</div>
-// 					<div className='nav-middle'>
-// 						<div className='nav-links'>
-// 							<div className='nav-links-inner'>
-// 								<Link
-// 									name=''
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === '' ? 'active' : ''
-// 									}`}
-// 									to='/'>
-// 									HOME
-// 								</Link>
-
-// 								<Link
-// 									name='new'
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === 'new' ? 'active' : ''
-// 									}`}
-// 									to='/new'>
-// 									NEW
-// 								</Link>
-// 								<Link
-// 									name='products'
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === 'products' ? 'active' : ''
-// 									}`}
-// 									to='/products'>
-// 									PRODUCTS
-// 								</Link>
-// 								<Link
-// 									name='custom'
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === 'custom' ? 'active' : ''
-// 									}`}
-// 									to='/custom'>
-// 									CUSTOM
-// 								</Link>
-// 								<Link
-// 									name='about'
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === 'about' ? 'active' : ''
-// 									}`}
-// 									to='/about'>
-// 									ABOUT
-// 								</Link>
-// 								<Link
-// 									name='careers'
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === 'careers' ? 'active' : ''
-// 									}`}
-// 									to='/careers'>
-// 									CONTACT
-// 								</Link>
-// 								<Link
-// 									name='careers'
-// 									onMouseDown={this.handleLinkClick}
-// 									className={`nav-link ${
-// 										this.state.currentPage === 'careers' ? 'active' : ''
-// 									}`}
-// 									to='/careers'>
-// 									CAREERS
-// 								</Link>
-// 							</div>
-// 						</div>
-// 						<div>
-// 							<form className='searchbar'>
-// 								<button onClick={this.handleCategories}>
-// 									FILTERS <i className='far fa-caret-square-down'></i>
-// 								</button>
-// 								<input
-// 									type='text'
-// 									onChange={this.updateSearch}
-// 									placeholder='Search for names and filters'
-// 								/>
-// 								<Link to={`/products/${this.state.searchQuery}`}>
-// 									<button type='submit'>
-// 										<i className='fas fa-search'></i>
-// 									</button>
-// 								</Link>
-// 							</form>
-// 							<Transition
-// 								items={this.state.catDisplayed}
-// 								from={{
-// 									opacity: 0,
-// 									position: 'fixed',
-// 								}}
-// 								enter={{
-// 									opacity: 1,
-// 									position: 'absolute',
-// 								}}
-// 								leave={{
-// 									opacity: 0,
-// 									position: 'fixed',
-// 								}}>
-// 								{show =>
-// 									show &&
-// 									(props => (
-// 										<CatDropdown onChange={this.updateFilter} style={props} />
-// 									))
-// 								}
-// 							</Transition>
-// 						</div>
-// 					</div>
-// 				</Responsive>
-// 				<Responsive as={Segment} maxWidth={1000}>
-// 					<div className='mobile-nav'>
-// 						<div className='mobile-nav-head'>
-// 							<div className='nav-brand'>JANZART</div>
-// 							<div className='nav-toggle' onMouseDown={this.handleClick}>
-// 								<i className='fas fa-bars'></i>
-// 							</div>
-// 						</div>
-
-// 						<form className='searchbar'>
-// 							<button onClick={this.handleCategories}>
-// 								FILTERS <i className='far fa-caret-square-down'></i>
-// 							</button>
-// 							<input type='text' onChange={this.updateSearch} />
-// 							<Link to={`/products/${this.state.searchQuery}`}>
-// 								<button type='submit'>
-// 									<i className='fas fa-search'></i>
-// 								</button>
-// 							</Link>
-// 						</form>
-// 						<Transition
-// 							items={this.state.catDisplayed}
-// 							from={{
-// 								opacity: 0,
-// 								position: 'fixed',
-// 							}}
-// 							enter={{
-// 								opacity: 1,
-// 								position: 'absolute',
-// 							}}
-// 							leave={{
-// 								opacity: 0,
-// 								position: 'fixed',
-// 							}}>
-// 							{show =>
-// 								show &&
-// 								(props => (
-// 									<CatDropdown onChange={this.updateFilter} style={props} />
-// 								))
-// 							}
-// 						</Transition>
-// 					</div>
-// 					<Transition
-// 						items={this.state.menuDisplayed}
-// 						from={{
-// 							opacity: 0,
-// 							transform: 'translateY(-500px)',
-// 							position: 'fixed',
-// 						}}
-// 						enter={{
-// 							opacity: 1,
-// 							transform: 'translateY(0px)',
-// 							position: 'fixed',
-// 						}}
-// 						leave={{
-// 							opacity: 0,
-// 							transform: 'translateY(-500px)',
-// 							position: 'fixed',
-// 						}}>
-// 						{show => show && (props => <Menu style={props} />)}
-// 					</Transition>
-// 				</Responsive>
-// 			</div>
-// 		)
-// 	}
-// }
-
 class Menu extends Component {
             state = {
               currentPage: ''
@@ -589,4 +334,4 @@ class Menu extends Component {
             }
 }
 
-export { searchQuery, catFilter }
+export { searchQueryGlobal, catFilterGlobal }
--- client/components/Products.jsx
+++ client/components/Products.jsx
@@ -2,70 +2,70 @@ import React, { Component } from 'react'
 import { animated, Spring } from 'react-spring/renderprops'
 import VisibilitySensor from 'react-visibility-sensor'
 import ItemCards from './ItemCards'
-import { searchQuery, catFilter } from './Navbar'
+import { searchQueryGlobal, catFilterGlobal } from './Navbar'
 class Products extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			searchQuery: '',
-			catFilter: [],
-		}
-	}
-	componentDidUpdate() {
-		if (
-			searchQuery != this.state.searchQuery ||
-			catFilter != this.state.catFilter
-		) {
-			console.log('changing search Query')
-			this.setState({ searchQuery: searchQuery, catFilter: catFilter })
-		}
-	}
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchQueryGlobal: '',
+      catFilterGlobal: []
+    }
+  }
+  componentDidUpdate () {
+    if (
+      searchQueryGlobal != this.state.searchQueryGlobal ||
+catFilterGlobal != this.state.catFilterGlobal
+    ) {
+      console.log('changing search Query')
+      this.setState({ searchQueryGlobal: searchQueryGlobal, catFilterGlobal: catFilterGlobal })
+    }
+  }
 
-	getTitle = () => {
-		if (searchQuery !== '') {
-			return `VIEWING ALL ${searchQuery.toUpperCase()}`
-		} else {
-			return 'VIEW ALL OUR ARTWORKS'
-		}
-	}
-	render() {
-		return (
-			<animated.div style={{ ...this.props.style }} className='page'>
-				<Spring
-					delay={1000}
-					from={{ transform: 'translateX(1000px)', opacity: 0 }}
-					to={{ transform: 'translateX(0px)', opacity: 1 }}
-					config={{ duration: 1000 }}
-				>
-					{props => (
-						<div className='container ' style={{ ...props }}>
-							<h1 className='home-featured-title'>
-								{this.getTitle()}
-								<hr />
-							</h1>
-						</div>
-					)}
-				</Spring>
+getTitle = () => {
+  if (searchQueryGlobal !== '') {
+    return `VIEWING ALL ${searchQueryGlobal.toUpperCase()}`
+  } else {
+    return 'VIEW ALL OUR ARTWORKS'
+  }
+}
+render () {
+  return (
+    <animated.div style={{ ...this.props.style }} className='page'>
+      <Spring
+        delay={1000}
+        from={{ transform: 'translateX(1000px)', opacity: 0 }}
+        to={{ transform: 'translateX(0px)', opacity: 1 }}
+        config={{ duration: 1000 }}
+      >
+        {props => (
+          <div className='container ' style={{ ...props }}>
+            <h1 className='home-featured-title'>
+              {this.getTitle()}
+              <hr />
+            </h1>
+          </div>
+        )}
+      </Spring>
 
-				<VisibilitySensor partialVisibility={true} minTopValue={50}>
-					{({ isVisible }) => {
-						return (
-							<div
-								className={`home-featured-items ${
-									isVisible ? 'visible' : 'hidden'
-								}`}
-							>
-								<ItemCards
-									searchFilter={this.state.searchQuery}
-									filter={this.state.catFilter}
-								/>
-							</div>
-						)
-					}}
-				</VisibilitySensor>
-			</animated.div>
-		)
-	}
+      <VisibilitySensor partialVisibility={true} minTopValue={50}>
+        {({ isVisible }) => {
+          return (
+            <div
+              className={`home-featured-items ${
+                isVisible ? 'visible' : 'hidden'
+              }`}
+            >
+              <ItemCards
+                searchFilter={this.state.searchQueryGlobal}
+                filter={this.state.catFilterGlobal}
+              />
+            </div>
+          )
+        }}
+      </VisibilitySensor>
+    </animated.div>
+  )
+}
 }
 
 export default Products
