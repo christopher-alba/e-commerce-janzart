92dba79b14b73cfab232c87d59d6d135b780bfc9
Christopher <calb200@aucklanduni.ac.nz>
2021-05-08 20:57:08 +1200

    reformat knexfile objects

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,49 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './server/db/dev.sqlite3'
-    }
+      filename: "./server/db/dev.sqlite3",
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     // The next line is where the application will read that environment variable to connect to the database
-    connection: process.env.DATABASE_URL + "?ssl=true",
-    ssl: { rejectUnauthorized: false },
+    connection: {
+      connectionString: process.env.DATABASE_URL + "?ssl=true",
+      ssl: { rejectUnauthorized: false },
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: __dirname + '/migrations'
+      directory: __dirname + "/migrations",
     },
     seeds: {
-      directory: __dirname + '/seeds'
-    }
+      directory: __dirname + "/seeds",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     // The next line is where the application will read that environment variable to connect to the database
-    connection: process.env.DATABASE_URL + "?ssl=true",
-    ssl: { rejectUnauthorized: false },
+    connection: {
+      connectionString: process.env.DATABASE_URL + "?ssl=true",
+      ssl: { rejectUnauthorized: false },
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: __dirname + '/migrations'
+      directory: __dirname + "/migrations",
     },
     seeds: {
-      directory: __dirname + '/seeds'
-    }
-  }
-
-}
+      directory: __dirname + "/seeds",
+    },
+  },
+};
